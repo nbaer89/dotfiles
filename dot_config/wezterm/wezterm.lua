b051093f899085ed4912145980041451d8de4b66
Nathan Baer <nbaer89@gmail.com>
2024-10-03 14:10:44 -0700

    fixed error

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 -- These are the basic's for using wezterm.
 -- Mux is the mutliplexes for windows etc inside of the terminal
 -- Action is to perform actions on the terminal
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 local act = wezterm.action
 
@@ -11,31 +11,32 @@ local keys = {}
 local mouse_bindings = {}
 local launch_menu = {}
 
--- This is for newer wezterm vertions to use the config builder 
+-- This is for newer wezterm vertions to use the config builder
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 config.window_background_opacity = 0.9
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.color_scheme = 'Rosé Pine (Gogh)'
+config.color_scheme = "Rosé Pine (Gogh)"
 -- This is my chosen font, we will get into installing fonts on windows later
-config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 11
 config.launch_menu = launch_menu
--- makes my cursor blink 
-config.default_cursor_style = 'BlinkingBar'
+-- makes my cursor blink
+config.default_cursor_style = "BlinkingBar"
 config.use_dead_keys = false
 config.disable_default_key_bindings = true
 config.initial_rows = 30
 config.initial_cols = 120
--- this adds the ability to use ctrl+v to paste the system clipboard 
+-- this adds the ability to use ctrl+v to paste the system clipboard
 config.keys = {
-  {
-    key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard'
-  },
-  { key = 'PageUp', action = wezterm.action.ScrollByPage(-1) },
-  { key = 'PageDown', action = wezterm.action.ScrollByPage(1) },
-
+	{
+		key = "v",
+		mods = "CTRL",
+		action = act.PasteFrom("Clipboard"),
+	},
+	{ key = "PageUp", action = wezterm.action.ScrollByPage(-1) },
+	{ key = "PageDown", action = wezterm.action.ScrollByPage(1) },
 }
 config.mouse_bindings = {
 	{
@@ -49,13 +50,13 @@ config.mouse_bindings = {
 			else
 				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
 			end
-		end),:/Users/nbaer/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe'
+		end),
 	},
 }
-config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe' }
+config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe" }
 
 -- Visual bell
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 config.visual_bell = {
 	target = "CursorColor",
 	fade_in_function = "EaseIn",
