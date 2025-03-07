5281692b43e37e822f25122e6acc29138eedd39b
Nathan Baer <nbaer89@gmail.com>
2025-03-07 14:51:33 -0800

    Add .chezmoiignore Add .config/nushell/.gitignore Remove .config/nushell/history.txt Add .config/nvim/.gitignore Remove .config/nvim/.gitignore Update .config/nvim/init.lua Remove .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/custom/plugins/init.lua Add .config/nvim/lua/kickstart/health.lua Add .config/nvim/lua/kickstart/plugins/autopairs.lua Add .config/nvim/lua/kickstart/plugins/debug.lua Add .config/nvim/lua/kickstart/plugins/gitsigns.lua Add .config/nvim/lua/kickstart/plugins/indent_line.lua Add .config/nvim/lua/kickstart/plugins/lint.lua Add .config/nvim/lua/kickstart/plugins/neo-tree.lua Remove .config/nvim/lua/plugins/chezmoi.lua Remove .config/nvim/lua/plugins/colorscheme.lua Remove .config/nvim/lua/plugins/example.lua Remove .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-	  { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-	  { "<leader>ps", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-  },
-}
