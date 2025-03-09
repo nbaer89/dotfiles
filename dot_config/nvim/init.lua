bc73b80c03888bbe99fa276b2244ac41f81d405c
Nathan Baer <nbaer89@gmail.com>
2025-03-09 11:25:02 -0700

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -905,11 +905,11 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"rose-pine/neovim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
+			require("rise-pine").setup({
 				styles = {
 					comments = { italic = false }, -- Disable italics in comments
 				},
@@ -918,7 +918,7 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("rose-pine")
 		end,
 	},
 
@@ -984,7 +984,12 @@ require("lazy").setup({
 				"markdown_inline",
 				"query",
 				"vim",
-				"vimdoc",
+				"vimdoc",,
+        "json",
+        "yaml",
+        "python",
+        "rust",
+        "toml",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
