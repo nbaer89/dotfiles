735957cefd31f9243d71d51ec54f1a15226935ac
Nathan Baer <nbaer89@gmail.com>
2025-03-30 09:22:37 -0700

    complete mason setup

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,18 @@
 return {
-	{
-    "williamboman/mason.nvim",
-	}
+    {
+	"williamboman/mason.nvim",
+	cmd = "Mason",
+	build = ":MasonUpdate",
+	opts_extend = {
+	    "esnure_installed",
+	},
+	opts = {
+	    ensure_installed = {
+		"stylua",
+		"shfmt",
+	    },
+	},
+	config = function()
+	    require("mason").setup()
+    }
 }
